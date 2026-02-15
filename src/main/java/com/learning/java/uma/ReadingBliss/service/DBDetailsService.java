e6649acbbbccf14af129c7d53005892e6aa3d52f
Uma Shankar <mr.umashankar@gmail.com>
2026-02-15 18:29:33 +0530

    Run cron job to call DB

---
+++ src/main/java/com/learning/java/uma/ReadingBliss/service/DBDetailsService.java
@@ -0,0 +1,26 @@
+package com.learning.java.uma.ReadingBliss.service;
+
+import com.learning.java.uma.ReadingBliss.dao.DBDetailsDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+
+@Component
+public class DBDetailsService {
+    @Autowired
+    private DBDetailsDao dbDetailsDao;
+
+    //  0 0 */12 * * * // Once in 12 hrs
+    @Scheduled(cron = "0 * * * * *")
+    public List<String> getTables(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss a", Locale.ENGLISH);
+        System.out.println(now.format(formatter) + ": Getting list of tables from public schema.");
+        return dbDetailsDao.getDBTables();
+    }
+}
